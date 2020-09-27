7a284a8c3b7e196a5b704b1f15be59bf194ae0df
Bemby Bergario <71754063+Bergario@users.noreply.github.com>
2020-09-27 23:32:51 +0700

    create TodoForm with Hooks

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = (e) => {
+    if (!value) {
+      alert("Todo List is Required!");
+      return;
+    }
+    e.preventDefault();
+    // addTodo(value);
+    alert(value);
+    setValue("");
+  };
   return (
     <section className="add">
-      <form action="" className="add-form">
-        <input type="text" className="add-input" />
+      <form action="" className="add-form" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button className="add-btn">Add</button>
       </form>
     </section>
   );
+};
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired
 };
 
 export default TodoForm;
